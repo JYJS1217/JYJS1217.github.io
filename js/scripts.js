4adb5f5336cddb9e6dbdb17fda4bdba9916f79f5
JYJS1217 <second_ekscn@naver.com>
2022-08-08 16:10:56 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -65,7 +65,8 @@ window.addEventListener('DOMContentLoaded', event => {
     var today = new Date();
     var gap = dday.getTime() - today.getTime();
     var result= Math.ceil(gap /(1000*60*60*24));
-    //document.write("D-day 까지" +result + "일 남았습니다.");
-    document.getElementById("count").innerHTML="D-day 까지" +result + "일 남았습니다."
+    
+    document.write("D-day " +result);
+    document.getElementById("count").innerHTML= "오늘은 "+today+" D-day 까지" +result + "일 남았습니다."
 });
 
