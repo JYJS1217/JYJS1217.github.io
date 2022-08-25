bb3a397c080cf654d9230acc5e4201e5c043bf58
JYJS1217 <second_ekscn@naver.com>
2022-08-26 00:50:06 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -155,7 +155,7 @@ $( 'button.callmember' ).click( function() {
 		blowAnimations: [ 'blow-soft-left', 'blow-medium-left'],
             className: 'sakura',
             //fallSpeed: 3,
-	    fallSpeed: 4,
+	    fallSpeed: 0.4,
             maxSize: 15,
             minSize: 10,
             newOn: 1300,
